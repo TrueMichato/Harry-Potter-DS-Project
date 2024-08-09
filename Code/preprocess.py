5c6f26b7f9b8e45a1028bc39b1e38127259a090b
Tomer Michaeli <tomer.michaeli2@cs.huji.ac.il>
2024-08-09 13:38:05 +0300

    Updated preprocess

--- Code/preprocess.py
+++ Code/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import nltk.tokenize as tk
+import re
 
 
 data = pd.read_csv("Data/harry_potter_books.csv")
@@ -7,8 +8,13 @@ data = pd.read_csv("Data/harry_potter_books.csv")
 book_texts = data.groupby(["book", "chapter"])["text"].apply(lambda x: " ".join(x)).reset_index()
 book_texts["chapter"] = book_texts["chapter"].apply(lambda x: int(x.split("-")[1]))
 book_texts = book_texts.sort_values(by=["book", "chapter"])
-book_texts["chapter name"] = book_texts["text"].apply(lambda x: x.split("  ")[0])
+book_texts = book_texts.reset_index(drop=True)
+# get chapter name using a regex
+book_texts["chapter name"] = book_texts["text"].str.extract(r'^([A-Z ]+)\s\s')
+# make chapter name a string
+book_texts["chapter name"] = book_texts["chapter name"].apply(lambda x: str(x))
 book_texts["text"] = book_texts.apply(lambda row: row['text'].replace(row['chapter name'], '').replace('  ', ' '), axis=1)
+
 
 # split text to sentences
 book_texts["sentences"] = book_texts["text"].apply(lambda x: tk.sent_tokenize(x))
