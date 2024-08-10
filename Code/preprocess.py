f0c367c61f78e886c2a2d908545daf28a5dc27bc
Tomer Michaeli <tomer.michaeli2@cs.huji.ac.il>
2024-08-10 11:41:47 +0300

    WIP adding correference, started fixing Mr and Mrs issues and examining need to switch names with ID before correference fix

--- Code/preprocess.py
+++ Code/preprocess.py
@@ -31,7 +31,7 @@ import re
 import spacy
 import coreferee
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_trf")
 nlp.add_pipe("coreferee")
 
 # def replace_pronouns_with_names(chapter_text: str, character_names) -> str:
@@ -93,12 +93,61 @@ def create_dict_names_id(df_characters) -> dict:
         dict_id_names[row["Id"]] = all_names
     return dict_id_names
 
-print(nlp.pipe_names)
+def reverse_dict(dict_names_id) -> dict:
+    dict_id_names = {}
+    for id_, names in dict_names_id.items():
+        for name in names:
+            if name in dict_id_names:
+                dict_id_names[name].append(id_)
+            else:
+                dict_id_names[name] = [id_]
+    return dict_id_names
+
+def solve_couple(text: str, names_dict: dict) -> str:
+    regex = re.compile(r"Mr\. and Mrs\. (\w+)")
+    match = regex.search(text)
+    if match:
+        couple_name = match.group(1)
+        name1 = "Mr. " + couple_name
+        name2 = "Mrs. " + couple_name
+        if name1 in names_dict and name2 in names_dict:
+            name1_id = "id_" + str(names_dict[name1][0])
+            name2_id = "id_" + str(names_dict[name2][0])
+            new_text = text.replace("Mr. and Mrs. " + couple_name, name1_id + " and " + name2_id)
+        else:
+            new_text = text.replace("Mr. and Mrs. " + couple_name, name1 + " and " + name2)
+        return new_text
+    return text
+
+def solve_mr_mrs(text: str, names_dict: dict, mr: bool=True) -> str:
+    regex = re.compile(r"Mr\. (\w+)") if mr else re.compile(r"Mrs\. (\w+)")
+    match = regex.search(text)
+    if match:
+        title = "Mr." if mr else "Mrs."
+        name = match.group(1)
+        name = title + name
+        if name in names_dict:
+            name_id = "id_" + str(names_dict[name][0])
+            new_text = text.replace(title + name, name_id)
+        else:
+            new_text = text
+        return new_text
+    return text
 
 
+# doc = nlp("213 and 214, of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because they just didn't hold with such nonsense. Harry Potter was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck, although he did have a very large mustache.")
+# doc._.coref_chains.print()
+# print(doc[0])
+# print(doc[26])
+# print(doc._.coref_chains.resolve(doc[0]))
+# print(doc._.coref_chains.resolve(doc[26]))
+# print(doc._.coref_chains.resolve())
 
-# df_characters = pd.read_csv("Data/character_names.csv")
-# dict_names_id = create_dict_names_id(df_characters)
+df_characters = pd.read_csv("Data/character_names.csv")
+dict_names_id = create_dict_names_id(df_characters)
+print(dict_names_id)
+dict_id_names = reverse_dict(dict_names_id)
+print(dict_id_names)
 # character_names = set([name for names in dict_names_id.values() for name in names])
 # df_text = pd.read_csv("Data/harry_potter_books_preprocessed.csv")
 # df_text["text"] = df_text["text"].apply(resolve_coreferences)
