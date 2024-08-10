958f657b91e71989f44d969321568e19d3a428b8
mika.li <mika.littor@mail.huji.ac.il>
2024-08-10 13:40:13 +0300

    use pickle for plotting

--- Code/simple_plot_connections.py
+++ Code/simple_plot_connections.py
@@ -4,7 +4,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import re
 import pickle
-import os
+
 
 def check_special_family_names(name, sentence):
     # making sure it won't catch the wrong family member cause this family names are associated
@@ -23,26 +23,27 @@ def check_special_family_names(name, sentence):
 
 
 def create_dict_connections(df_sentences, dict_names_id):
-        def find_character_id_pairs(sentence, dict_names_id):
-            present_ids = []
-            for char_id, names in dict_names_id.items():
-                for name in names:
-                    pattern = r'\b' + re.escape(name) + r'\b'
-                    match = re.search(pattern, sentence)
-                    if match:
-                        if name in ["Potter", "Weasley", "Malfoy"]:
-                            if not check_special_family_names(name, sentence):
-                                break
-                        present_ids.append(char_id)
-                        break  # Break after the first match to avoid double counting
-            return list(set(combinations(sorted(present_ids), 2)))
+    def find_character_id_pairs(sentence, dict_names_id):
+        present_ids = []
+        for char_id, names in dict_names_id.items():
+            for name in names:
+                pattern = r'\b' + re.escape(name) + r'\b'
+                match = re.search(pattern, sentence)
+                if match:
+                    if name in ["Potter", "Weasley", "Malfoy"]:
+                        if not check_special_family_names(name, sentence):
+                            break
+                    present_ids.append(char_id)
+                    break  # Break after the first match to avoid double counting
+        return list(set(combinations(sorted(present_ids), 2)))
 
-        df_sentences['character_pairs'] = df_sentences['sentence'].apply(
-            lambda x: find_character_id_pairs(x, dict_names_id))
-        df_exploded = df_sentences.explode('character_pairs').dropna(subset=['character_pairs'])
+    df_sentences['character_pairs'] = df_sentences['sentence'].apply(
+        lambda x: find_character_id_pairs(x, dict_names_id))
+    df_exploded = df_sentences.explode('character_pairs').dropna(subset=['character_pairs'])
 
-        pair_counts = df_exploded['character_pairs'].value_counts().to_dict()
-        return pair_counts
+    pair_counts = df_exploded['character_pairs'].value_counts().to_dict()
+    return pair_counts
+
 
 def create_dict_names_id(df_characters):
     dict_id_names = {}
@@ -54,10 +55,12 @@ def create_dict_names_id(df_characters):
         dict_id_names[row['Id']] = all_names
     return dict_id_names
 
+
 def check_add_node(G, name):
     if name not in G.nodes:
         G.add_node(name)
     return G
+
 
 def plot_simple_connections(pair_counts, dict_names_id, threshold_count=3):
     G = nx.Graph()
@@ -73,7 +76,8 @@ def plot_simple_connections(pair_counts, dict_names_id, threshold_count=3):
 
     pos = nx.circular_layout(G)
     plt.figure(figsize=(20, 20))
-    nx.draw(G, pos, with_labels=True, node_size=3000, node_color="skyblue", font_size=10, font_weight="bold", edge_color="gray", font_color='black',
+    nx.draw(G, pos, with_labels=True, node_size=3000, node_color="skyblue", font_size=10, font_weight="bold",
+            edge_color="gray", font_color='black',
             edgecolors="black", linewidths=1, alpha=0.7, width=2, connectionstyle='arc3, rad = 0.1', arrows=True)
     plt.show()
 
@@ -90,23 +94,31 @@ def remove_characters_below_threshold(dict_names_id, df_sentences, threshold=2):
     return filtered_dict
 
 
+def plot_edge_size_connections(pair_counts, dict_names_id, threshold_count=3):
+
+
+
 def save_pair_counts(pair_counts):
     with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\pair_counts.pkl", "wb") as f:
         pickle.dump(pair_counts, f)
+
 
 def get_pair_counts_from_pickle():
     with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\pair_counts.pkl", "rb") as f:
         pair_counts = pickle.load(f)
     return pair_counts
 
+
 def save_dict_names_id(dict_names_id):
     with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\dict_names_id.pkl", "wb") as f:
         pickle.dump(dict_names_id, f)
+
 
 def get_dict_names_id_from_pickle():
     with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\dict_names_id.pkl", "rb") as f:
         dict_names_id = pickle.load(f)
     return dict_names_id
+
 
 def main():
     # todo: remove the pickle usage in the future
@@ -122,9 +134,11 @@ def main():
     pair_counts = get_pair_counts_from_pickle()
     plot_simple_connections(pair_counts, dict_names_id, threshold_count=10)
 
+    plot_edge_size_connections(pair_counts, dict_names_id, threshold_count=10)
+
     # todo: Louvain for community detection for the plot
     # pagerank
 
+
 if __name__ == "__main__":
     main()
-
