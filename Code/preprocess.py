e93f4142abbbfbab6d11fc988d44ced24688fef7
Tomer Michaeli <tomer.michaeli2@cs.huji.ac.il>
2024-08-10 13:31:04 +0300

    WIP adding correference, only correference left

--- Code/preprocess.py
+++ Code/preprocess.py
@@ -34,51 +34,16 @@ import coreferee
 nlp = spacy.load("en_core_web_trf")
 nlp.add_pipe("coreferee")
 
-# def replace_pronouns_with_names(chapter_text: str, character_names) -> str:
-#     doc = nlp(chapter_text)
-
-#     # Dictionary to store replacements
-#     replacements = {}
-
-#     # Iterate over coreference chains
-#     for chain in doc._.coref_chains:
-#         # Find the main entity that is a character's name
-#         for mention in chain:
-#             mention_text = doc[mention.start:mention.end].text
-#             if mention_text in character_names:
-#                 character_name = mention_text
-#                 break
-#         else:
-#             # If no character name is found in the chain, skip to the next one
-#             continue
-
-#         # Replace pronouns in the chain with the character's name
-#         for mention in chain:
-#             mention_text = doc[mention.start:mention.end].text
-#             if mention_text.lower() in ["he", "she", "him", "her", "his", "hers", "they", "them"]:
-#                 replacements[(mention.start, mention.end)] = character_name
-
-#     # Replace text in the original document
-#     new_text = list(doc)
-#     for (start, end), name in replacements.items():
-#         new_text[start:end] = [name]
-
-#     return " ".join([token.text for token in new_text])
-
-
 def resolve_coreferences(chapter_text) -> str:
     doc = nlp(chapter_text)
-
-    # Get the coreference clusters
-    clusters = doc._.coref_clusters
+    chains = doc._.coref_chains
 
     # Create a mapping from pronouns to resolved mentions
     resolved_text = chapter_text
-    for cluster in clusters:
-        main_entity = cluster.main.text
-        for mention in cluster.mentions:
-            if mention != cluster.main:
-                resolved_text = resolved_text.replace(mention.text, main_entity)
+    for chain in chains:
+        for mention in chain:
+            fix = doc._.coref_chains.resolve(mention)
+            resolved_text = resolved_text.replace(mention.text, fix)
 
     return resolved_text
 
@@ -103,55 +68,72 @@ def reverse_dict(dict_names_id) -> dict:
                 dict_id_names[name] = [id_]
     return dict_id_names
 
-def solve_couple(text: str, names_dict: dict) -> str:
+def solve_couple(text: str) -> str:
     regex = re.compile(r"Mr\. and Mrs\. (\w+)")
     match = regex.search(text)
     if match:
         couple_name = match.group(1)
-        name1 = "Mr. " + couple_name
-        name2 = "Mrs. " + couple_name
-        if name1 in names_dict and name2 in names_dict:
-            name1_id = "id_" + str(names_dict[name1][0])
-            name2_id = "id_" + str(names_dict[name2][0])
-            new_text = text.replace("Mr. and Mrs. " + couple_name, name1_id + " and " + name2_id)
-        else:
-            new_text = text.replace("Mr. and Mrs. " + couple_name, name1 + " and " + name2)
+        name1 = "Mr._" + couple_name
+        name2 = "Mrs._" + couple_name
+        new_text = text.replace("Mr. and Mrs. " + couple_name, name1 + " and " + name2)
         return new_text
     return text
 
-def solve_mr_mrs(text: str, names_dict: dict, mr: bool=True) -> str:
+def solve_mr_mrs(text: str, mr: bool=True) -> str:
     regex = re.compile(r"Mr\. (\w+)") if mr else re.compile(r"Mrs\. (\w+)")
     match = regex.search(text)
     if match:
         title = "Mr." if mr else "Mrs."
         name = match.group(1)
         name = title + name
-        if name in names_dict:
-            name_id = "id_" + str(names_dict[name][0])
-            new_text = text.replace(title + name, name_id)
-        else:
-            new_text = text
+        new_text = text.replace(title + " " + name, title + "_" + name)
         return new_text
     return text
 
+def solve_mrs(text: str) -> str:
+    return solve_mr_mrs(text, False)
 
-# doc = nlp("213 and 214, of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because they just didn't hold with such nonsense. Harry Potter was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck, although he did have a very large mustache.")
-# doc._.coref_chains.print()
-# print(doc[0])
-# print(doc[26])
-# print(doc._.coref_chains.resolve(doc[0]))
-# print(doc._.coref_chains.resolve(doc[26]))
-# print(doc._.coref_chains.resolve())
+def solve_mrs_mr_issue(text: str) -> str:
+    text = solve_mr_mrs(text)
+    return solve_mrs(text)
 
-df_characters = pd.read_csv("Data/character_names.csv")
-dict_names_id = create_dict_names_id(df_characters)
-print(dict_names_id)
-dict_id_names = reverse_dict(dict_names_id)
-print(dict_id_names)
+def solve_long_name_issue(text: str, names: list) -> str:
+    for name in names:
+        if len(name.split(" ")) > 2:
+            new_name = "__".join(name.split(" "))
+            text = re.sub(rf"\b{name}\b", new_name, text)
+    return text
+
+
+doc = nlp("Mr.__Dursley and Mrs.__Dursley, of number four, Privet Drive, were proud to say that they were perfectly normal, thank you very much. They were the last people you'd expect to be involved in anything strange or mysterious, because they just didn't hold with such nonsense. Mr.__Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck, although he did have a very large mustache.")
+doc._.coref_chains.print()
+for chain in doc._.coref_chains:
+    for mention in chain:
+        if len(mention) > 1:
+            for m in mention:
+                fix = doc._.coref_chains.resolve(doc[m])
+                print(fix)
+        else:
+            fix = doc._.coref_chains.resolve(doc[mention.token_indexes[0]])
+            print(fix)
+        # fix = doc._.coref_chains.resolve(doc[mention])
+        # print(f"{mention.text} -> {fix}")
+print(doc[0])
+print(doc[26])
+print(doc._.coref_chains.resolve(doc[0]))
+print(doc._.coref_chains.resolve(doc[26]))
+print(doc._.coref_chains.resolve())
+
+# df_characters = pd.read_csv("Data/character_names.csv")
+# dict_names_id = create_dict_names_id(df_characters)
 # character_names = set([name for names in dict_names_id.values() for name in names])
 # df_text = pd.read_csv("Data/harry_potter_books_preprocessed.csv")
-# df_text["text"] = df_text["text"].apply(resolve_coreferences)
-# )
+
+# df_text["text"] = df_text["text"].apply(lambda x: solve_couple(x))
+# df_text["text"] = df_text["text"].apply(lambda x: solve_mrs_mr_issue(x))
+# df_text["text"] = df_text["text"].apply(lambda x: solve_long_name_issue(x, character_names))
+
+
 # df_text["sentences"] = df_text["text"].apply(lambda x: tk.sent_tokenize(x))
 # print(df_text["text"][0])
 # print(df_text["sentences"][0])
