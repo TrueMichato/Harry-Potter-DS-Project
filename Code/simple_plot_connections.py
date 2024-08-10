fb7227711ccdb181760c417d234375544d21b08a
mika.li <mika.littor@mail.huji.ac.il>
2024-08-10 13:35:37 +0300

    use pickle for plotting

--- Code/simple_plot_connections.py
+++ Code/simple_plot_connections.py
@@ -3,24 +3,8 @@ from itertools import combinations
 import networkx as nx
 import matplotlib.pyplot as plt
 import re
-
-
-# def create_dict_connections(df_sentences, dict_names_id):
-#     def find_character_id_pairs(sentence, dict_names_id):
-#         present_ids = []
-#         for char_id, names in dict_names_id.items():
-#             for name in names:
-#                 pattern = r'\b' + re.escape(name) + r'\b'
-#                 if re.search(pattern, sentence):
-#                     present_ids.append(char_id)
-#                     break  # Break after the first match to avoid double counting
-#         return list(set(combinations(sorted(present_ids), 2)))
-#
-#     df_sentences['character_pairs'] = df_sentences['sentence'].apply(lambda x: find_character_id_pairs(x, dict_names_id))
-#     df_exploded = df_sentences.explode('character_pairs').dropna(subset=['character_pairs'])
-#
-#     pair_counts = df_exploded['character_pairs'].value_counts().to_dict()
-#     return pair_counts
+import pickle
+import os
 
 def check_special_family_names(name, sentence):
     # making sure it won't catch the wrong family member cause this family names are associated
@@ -105,14 +89,37 @@ def remove_characters_below_threshold(dict_names_id, df_sentences, threshold=2):
 
     return filtered_dict
 
-def main():
-    df_sentences = pd.read_csv(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\harry_potter_sentences.csv")
-    df_characters = pd.read_csv(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\character_names.csv")
-    dict_names_id = create_dict_names_id(df_characters)
-    dict_names_id = remove_characters_below_threshold(dict_names_id, df_sentences, threshold=14)
-    pair_counts = create_dict_connections(df_sentences, dict_names_id)
 
-    # print(pair_counts)
+def save_pair_counts(pair_counts):
+    with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\pair_counts.pkl", "wb") as f:
+        pickle.dump(pair_counts, f)
+
+def get_pair_counts_from_pickle():
+    with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\pair_counts.pkl", "rb") as f:
+        pair_counts = pickle.load(f)
+    return pair_counts
+
+def save_dict_names_id(dict_names_id):
+    with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\dict_names_id.pkl", "wb") as f:
+        pickle.dump(dict_names_id, f)
+
+def get_dict_names_id_from_pickle():
+    with open(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\dict_names_id.pkl", "rb") as f:
+        dict_names_id = pickle.load(f)
+    return dict_names_id
+
+def main():
+    # todo: remove the pickle usage in the future
+    # df_sentences = pd.read_csv(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\harry_potter_sentences.csv")
+    # df_characters = pd.read_csv(r"C:\Users\mikal\Documents\CSMSE\needle\final_project\Data\character_names.csv")
+    # dict_names_id = create_dict_names_id(df_characters)
+    # dict_names_id = remove_characters_below_threshold(dict_names_id, df_sentences, threshold=14)
+    # save_dict_names_id(dict_names_id)
+    # pair_counts = create_dict_connections(df_sentences, dict_names_id)
+    # save_pair_counts(pair_counts)
+
+    dict_names_id = get_dict_names_id_from_pickle()
+    pair_counts = get_pair_counts_from_pickle()
     plot_simple_connections(pair_counts, dict_names_id, threshold_count=10)
 
     # todo: Louvain for community detection for the plot
