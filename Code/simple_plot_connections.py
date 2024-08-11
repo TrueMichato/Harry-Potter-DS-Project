84049486c73aae256884ff5a29d72834581f726d
mika.li <mika.littor@mail.huji.ac.il>
2024-08-11 16:51:42 +0300

    pickle

--- Code/simple_plot_connections.py
+++ Code/simple_plot_connections.py
@@ -321,13 +321,13 @@ def get_dict_names_id_from_pickle():
 
 def main():
     # todo: remove the pickle usage in the future
-    # df_sentences = pd.read_csv(r"..\Data\harry_potter_sentences.csv")
-    # df_characters = pd.read_csv(r"..\Data\character_names.csv")
-    # dict_names_id = create_dict_names_id(df_characters)
-    # dict_names_id = remove_characters_below_threshold(dict_names_id, df_sentences, threshold=14)
-    # save_dict_names_id(dict_names_id)
-    # pair_counts = create_dict_connections(df_sentences, dict_names_id)
-    # save_pair_counts(pair_counts)
+    df_sentences = pd.read_csv(r"..\Data\harry_potter_sentences.csv")
+    df_characters = pd.read_csv(r"..\Data\character_names.csv")
+    dict_names_id = create_dict_names_id(df_characters)
+    dict_names_id = remove_characters_below_threshold(dict_names_id, df_sentences, threshold=16)
+    save_dict_names_id(dict_names_id)
+    pair_counts = create_dict_connections(df_sentences, dict_names_id)
+    save_pair_counts(pair_counts)
 
     dict_names_id = get_dict_names_id_from_pickle()
     pair_counts = get_pair_counts_from_pickle()
