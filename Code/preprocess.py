ce544b782b95b1de9b91b3e2c9dddf3f1d328997
mayacohen7 <maya.cohen9@mail.huji.ac.il>
2024-08-13 11:09:23 +0300

    j

--- Code/preprocess.py
+++ Code/preprocess.py
@@ -3,12 +3,9 @@ import nltk.tokenize as tk
 import re
 import spacy
 import coreferee
+import tqdm
 
-nlp = spacy.load("en_core_web_trf")
-nlp.add_pipe("coreferee")
-
-
-def resolve_coreferences(chapter_text) -> str:
+def resolve_coreferences(chapter_text: str) -> str:
     doc = nlp(chapter_text)
     text_broken = [token.text for token in doc]
 
@@ -147,35 +144,119 @@ def preprocess_text(text: str, chapter: int, book: str, character_names: list) -
     return text
 
 
-data = pd.read_csv("Data/harry_potter_books.csv")
-df_characters = pd.read_csv("Data/character_names.csv")
-dict_names_id = create_dict_names_id(df_characters)
-character_names = set([name for names in dict_names_id.values() for name in names])
-
-book_texts = (
-    data.groupby(["book", "chapter"])["text"].apply(lambda x: " ".join(x)).reset_index()
-)
-book_texts["chapter"] = book_texts["chapter"].apply(lambda x: int(x.split("-")[1]))
-book_texts = book_texts.sort_values(by=["book", "chapter"])
-book_texts = book_texts.reset_index(drop=True)
-book_texts["text"] = book_texts.apply(lambda row: preprocess_text(row["text"], row["chapter"], row["book"]), axis=1)
-# # get chapter name using a regex
-# #  book_texts["chapter name"] = book_texts["text"].str.extract(r'^([A-Z ]+)\s\s')
-# # # make chapter name a string
-# # book_texts["chapter name"] = book_texts["chapter name"].apply(lambda x: str(x))
-# # book_texts["text"] = book_texts.apply(lambda row: row['text'].replace(row['chapter name'], '').replace('  ', ' '), axis=1)
+def fix_spacing(text: str, names: list) -> str:
+    global i
+    i += 1
+    print(f"{i}/{length}")
+    for name in tqdm.tqdm(names):
+        text = re.sub(
+            rf"((?<=[a-z]){re.escape(name)})|({re.escape(name)}(?<=[a-z]))",
+            r" " + name,
+            text,
+        )
+    return text
 
 
-# split text to sentences
-book_texts["sentences"] = book_texts["text"].apply(lambda x: tk.sent_tokenize(x))
-print(book_texts.head(15))
+def fix_unique_names() -> None:
+    df_characters = pd.read_csv("Data/character_names.csv")
+    p_name = df_characters["Name"].apply(lambda x: x.split(" ")[0])
 
-book_texts.to_csv("Data/harry_potter_books_preprocessed.csv", index=False)
+    unique_mask = p_name.duplicated(keep=False) == False
 
-# create a list of all sentences, without book or chapter affiliation
-all_sentences = [
-    sentence for sentences in book_texts["sentences"] for sentence in sentences
-]
-all_sentences_df = pd.DataFrame(all_sentences, columns=["sentence"])
+    df_characters.loc[unique_mask, "Other Names"] = (
+        df_characters.loc[unique_mask, "Other Names"].fillna("")
+        + df_characters.loc[unique_mask, "Other Names"].apply(
+            lambda x: ", " if x else ""
+        )
+        + p_name[unique_mask]
+    )
 
-all_sentences_df.to_csv("Data/harry_potter_sentences.csv", index=False)
+    # Remove leading/trailing whitespace and commas
+    df_characters["Other Names"] = df_characters["Other Names"].str.strip(", ")
+    df_characters["Other Names"] = df_characters["Other Names"].str.replace(" , ", ", ")
+    df_characters["Other Names"] = df_characters["Other Names"].fillna("")
+    df_characters["Other Names"] = (
+        df_characters["Other Names"].str.split(", ").apply(lambda x: ", ".join(set(x)))
+    )
+
+    df_characters.to_csv("Data/character_names.csv", index=False)
+
+
+def get_character_names() -> list:
+    df_characters = pd.read_csv("Data/character_names.csv")
+
+    dict_names_id = create_dict_names_id(df_characters)
+    character_names = set([name for names in dict_names_id.values() for name in names])
+    return character_names
+
+
+def create_preprocessed_data() -> None:
+    data = pd.read_csv("Data/harry_potter_books.csv")
+    character_names = get_character_names()
+
+    book_texts = (
+        data.groupby(["book", "chapter"])["text"]
+        .apply(lambda x: " ".join(x))
+        .reset_index()
+    )
+    book_texts["chapter"] = book_texts["chapter"].apply(lambda x: int(x.split("-")[1]))
+    book_texts = book_texts.sort_values(by=["book", "chapter"])
+    book_texts = book_texts.reset_index(drop=True)
+    print(book_texts.head(15))
+    book_texts.to_csv("Data/harry_potter_books_test.csv", index=False)
+    book_texts["text"] = book_texts.apply(
+        lambda row: preprocess_text(
+            row["text"], row["chapter"], row["book"], character_names
+        ),
+        axis=1,
+    )
+    ### get chapter name using a regex - dropped because data was inconsistent ###
+    #  book_texts["chapter name"] = book_texts["text"].str.extract(r'^([A-Z ]+)\s\s')
+    # # make chapter name a string
+    # book_texts["chapter name"] = book_texts["chapter name"].apply(lambda x: str(x))
+    # book_texts["text"] = book_texts.apply(lambda row: row['text'].replace(row['chapter name'], '').replace('  ', ' '), axis=1)
+
+    # split text to sentences
+    book_texts["sentences"] = book_texts["text"].apply(lambda x: tk.sent_tokenize(x))
+    print(book_texts.head(15))
+
+    book_texts.to_csv("Data/harry_potter_books_preprocessed.csv", index=False)
+
+
+def create_sentences_data(fix_text: bool = False) -> None:
+    preprocessed_data = pd.read_csv("Data/harry_potter_books_preprocessed.csv")
+    if fix_text:
+        character_names = get_character_names()
+        global length, i
+        length = len(preprocessed_data.index)
+        i = 0
+
+        preprocessed_data["text"] = preprocessed_data["text"].apply(
+            lambda x: fix_spacing(x, character_names)
+        )
+        preprocessed_data.to_csv(
+            "Data/harry_potter_books_preprocessed.csv", index=False
+        )
+    preprocessed_data["sentences"] = preprocessed_data["text"].apply(
+        lambda x: tk.sent_tokenize(x)
+    )
+
+    preprocessed_data.to_csv("Data/harry_potter_books_preprocessed.csv", index=False)
+
+    # create a list of all sentences, without book or chapter affiliation
+    all_sentences = [
+        sentence
+        for sentences in preprocessed_data["sentences"]
+        for sentence in sentences
+    ]
+    all_sentences_df = pd.DataFrame(all_sentences, columns=["sentence"])
+
+    all_sentences_df.to_csv("Data/harry_potter_sentences.csv", index=False)
+
+
+if __name__ == "__main__":
+    nlp = spacy.load("en_core_web_trf")
+    nlp.add_pipe("coreferee")
+    # fix_unique_names()
+    # create_preprocessed_data()
+    create_sentences_data()
